a9853f3dfeffab1fd807fc2d58511e913afcd2f9
Kimberly Hargrave <kitdh2004@gmail.com>
2024-11-03 19:11:16 -0500

    NewEnemyType

---
+++ Fighter Planes/Assets/Scripts/Enemy2.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, -0.5f, 0) * Time.deltaTime * 1.5f);
+        //Comes down faster and spawns sooner
+
+        if (transform.position.y < -8.5f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
