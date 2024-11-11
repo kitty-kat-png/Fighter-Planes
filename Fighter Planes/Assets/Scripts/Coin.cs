5c2bd0a3903fc61fa79d0f43e6cc4e822674dace
Kimberly Hargrave <kitdh2004@gmail.com>
2024-11-11 14:33:09 -0500

    Coin
    Added coin prefab, script, and modified scripts

---
+++ Fighter Planes/Assets/Scripts/Coin.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(this.gameObject, 2);
+    }
+
+    private void OnTriggerEnter2D(Collider2D whatIHit)
+    {
+        if (whatIHit.tag == "Player")
+        {
+            //Coin collected!
+            GameObject.Find("GameManager").GetComponent<GameManager>().EarnScore(1);
+            Destroy(this.gameObject);
+        }
+    }
+    
+}
--- Fighter Planes/Assets/Scripts/GameManager.cs
+++ Fighter Planes/Assets/Scripts/GameManager.cs
@@ -1,19 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
 
     public GameObject player;
-    public GameObject enemy;
-    public GameObject enemy2;
+    public GameObject enemyOne;
+    public GameObject cloud;
+    public GameObject coin;
+
+    public TextMeshProUGUI scoreText;
+
+    private int score;
 
     // Start is called before the first frame update
     void Start()
     {
         Instantiate(player, transform.position, Quaternion.identity);
-        InvokeRepeating("CreateEnemy", 1f, 3f);
+        InvokeRepeating("CreateEnemyOne", 1f, 3f);
+        InvokeRepeating("CreateCoin", 1f, 3f);
+        CreateSky();
+        score = 0;
+        scoreText.text = "Score: " + score;
     }
 
     // Update is called once per frame
@@ -22,10 +33,27 @@ public class GameManager : MonoBehaviour
         
     }
 
-    void CreateEnemy()
+    void CreateEnemyOne()
     {
-        Instantiate(enemy, new Vector3(Random.Range(-9f, 9f), 9f, 0), Quaternion.identity);
-        Instantiate(enemy2, new Vector3(Random.Range(-9f, 9f), 9f, 0), Quaternion.identity);
+        Instantiate(enemyOne, new Vector3(Random.Range(-9f, 9f), 7.5f, 0), Quaternion.Euler(0, 0, 180));
+    }
 
+    void CreateCoin()
+    {
+        Instantiate(coin, new Vector3(Random.Range(-9f, 9f), 7.5f, 0), Quaternion.Euler(0, 0, 180));
+    }
+
+    void CreateSky()
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            Instantiate(cloud, transform.position, Quaternion.identity);
+        }
+    }
+
+    public void EarnScore(int newScore)
+    {
+        score = score + newScore;
+        scoreText.text = "Score: " + score;
     }
 }
