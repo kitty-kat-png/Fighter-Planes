f28d8a8835f590f7b2b8acd4c5adba6fbfb10e24
Kimberly Hargrave <kitdh2004@gmail.com>
2024-11-10 20:31:47 -0500

    Commit Game Manager
    Did it wrong initially. Fixed!

--- Fighter Planes/Assets/Scripts/GameManager.cs
+++ Fighter Planes/Assets/Scripts/GameManager.cs
@@ -7,6 +7,7 @@ public class GameManager : MonoBehaviour
 
     public GameObject player;
     public GameObject enemy;
+    public GameObject enemy2;
 
     // Start is called before the first frame update
     void Start()
