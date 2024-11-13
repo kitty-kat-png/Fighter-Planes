e6d69050c8f0cbabbe11ee6ef136cd727e921238
ciera628 <143846698+ciera628@users.noreply.github.com>
2024-11-13 15:44:28 -0500

    Update GameManager.cs

--- Fighter Planes/Assets/Scripts/GameManager.cs
+++ Fighter Planes/Assets/Scripts/GameManager.cs
@@ -18,11 +18,49 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void CreateEnemy()
     {
         Instantiate(enemy, new Vector3(Random.Range(-9f, 9f), 9f, 0), Quaternion.identity);
     }
-}
+
+
+
+    private void Start()
+        {
+          gameOver = false;
+          PlayerPrefs.SetInt("Lives", 3); //initial lives
+        }
+
+    public int lives;
+
+      void start ()
+      {
+        updateui();
+      }
+
+      void updateui ()
+      {
+        Lives = PlayerPrefs.GetInt("Lives");
+         GameObject.Find("livesUI").GetComponent<Text>().text = "Lives : " + Lives;
+      }
+
+      void OnCollisionEnter(Collision collision)
+      {
+        if (collision.GameObject.CompareTag("Enemy"))
+        {
+          lives -= 1;
+          lives = PlayerPrefs.GetInt("lives")
+           PlayerPrefs.SetInt("Lives", lives);
+
+        if (lives <= 0)
+        {
+          gameOver = true
+          Debug.Log ("Game Over")
+          Destory (this.GameObject);
+        }
+      }
+
+    }
