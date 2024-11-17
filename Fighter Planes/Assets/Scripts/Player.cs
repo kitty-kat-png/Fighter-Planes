f985c7baec2208423c74d2bbc3754a143a375d58
ciera628 <143846698+ciera628@users.noreply.github.com>
2024-11-17 12:13:53 -0500

    Update Player.cs

--- Fighter Planes/Assets/Scripts/Player.cs
+++ Fighter Planes/Assets/Scripts/Player.cs
@@ -7,12 +7,18 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+      {
+          PlayerPrefs.SetInt("Lives", 3); //setting players initial lives.
+      }
+  }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      Lives = PlayerPrefs.GetInt("Lives");
+
+ GameObject.Find("livesUI").GetComponent<Text>().text = "Lives : " + Lives;
+}
     }
 }
